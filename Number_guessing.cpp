7969e87bbfd9322adeed26378336eb7cea4577a3
Anugya Bajpai <97664896+AnugyaBajpai2454@users.noreply.github.com>
2024-04-19 13:35:17 +0530

    Update Number_guessing.cpp

--- Number_guessing.cpp
+++ Number_guessing.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+int main()
+{
+  srand((unsigned int) time(NULL));
+  int Number_To_Guess = (rand() % 100) + 1; 
+  int Guess = 0;
+}
