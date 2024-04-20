4a98565024956695d3157037272f51476b447feb
Anugya Bajpai <97664896+AnugyaBajpai2454@users.noreply.github.com>
2024-04-20 21:31:27 +0530

    Final commit - Number_guessing.cpp

--- Number_guessing.cpp
+++ Number_guessing.cpp
@@ -8,4 +8,17 @@ int main()
   srand((unsigned int) time(NULL));
   int Number_To_Guess = (rand() % 100) + 1; 
   int Guess = 0;
+  do
+  {
+    cout << "Enter Guess (Enter the number you think the code will take as random guess) (1-100): ";
+    cin >> Guess;
+    if (Guess > Number_To_Guess)
+      cout << "Guess lower!" << endl;
+    else if (Guess < Number_To_Guess)
+      cout << "Guess higher!" << endl;
+    else
+      cout << "You won!" << endl;
+  } while (Guess != Number_To_Guess);
+   
+  return 0;
 }
